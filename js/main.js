f0619f9d9dee1c17bb61d15035cc4be9819f451d
Alexey <koroza.alexey99@gmail.com>
2022-02-24 20:35:49 +0300

    *

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $(function () {
   $('.design_slider').slick({
     dots: true,
     slidesToShow: 4,
-    variableWidth: true
+    variableWidth: true,
+    prevArrow:
+      '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="arrow-left.svg" />',
+    nextArrow:
+      '<img class="arrow arrow_right" src="images/arrow-right.svg" alt="arrow-right.svg" />',
   });
 });
